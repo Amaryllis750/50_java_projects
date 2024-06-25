eb0ce98638d2a7b1ca45eb779e5b86e3f204a6ed
Lavendar <danielchukwuemela751@gmail.com>
2024-06-25 21:57:59 +0100

    Word Counting Tool
    Just finished the GUI of the app
    
    Checking if there are any errors

--- Word Couting Tool/src/AppGUI.java
+++ Word Couting Tool/src/AppGUI.java
@@ -1,3 +1,48 @@
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+
 public class AppGUI {
-    
+    private JTextArea displayArea;
+
+    public void createGUI(){
+        JFrame frame = new JFrame("Word counting tool");
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+        displayArea = new JTextArea(10, 20);
+        displayArea.setLineWrap(true);
+        displayArea.setEnabled(false);
+        
+        JScrollPane scroller = new JScrollPane(displayArea);
+        scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        mainPanel.add(scroller);
+
+        JButton showButton = new JButton("Show Count");
+        showButton.addActionListener(event -> showCount());
+
+        mainPanel.add(showButton);
+
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(500, 600);
+        frame.setVisible(true);
+    }
+
+    public void showCount(){
+        // get the counts from the Counter class
+        Map<String, Integer> counts = Counter.parseString();
+        int wordCount = counts.get("Words");
+        int charCount = counts.get("Special Characters");
+        int lineCount = counts.get("Lines");
+        int numCount = counts.get("Digits");
+
+        String text = "Words: " + wordCount + "\n";
+        text += "Special Characters: " + charCount + "\n";
+        text += "Lines: " + lineCount + "\n";
+        text += "Digits: " + numCount;
+
+        displayArea.setText(text);
+    }
 }
