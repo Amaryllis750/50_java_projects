b0ff4ab68c4b4e9025613293e315ca70e6a3b786
Lavendar <danielchukwuemela751@gmail.com>
2024-07-17 20:25:46 +0100

    Just started the scientific calculator project

---
+++ calculator/src/main/java/com/calculator/gui/CalculatorGUI.java
@@ -0,0 +1,12 @@
+package com.calculator.gui;
+
+import javax.swing.*;
+
+
+public class CalculatorGUI extends JFrame{
+    
+    public CalculatorGUI(){
+        super("Calculator");
+        
+    }
+}
