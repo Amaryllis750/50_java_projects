c60429209af0fc00f1227eadee60c55369194563
Nkemakolam Okoronkwo <danielchukwuemela751@gmail.com>
2024-09-26 12:37:59 +0100

    Restarting the project

--- AdventureLog/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.daniel.adventurelog.adventurelog {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.daniel.adventurelog.adventurelog to javafx.fxml;
-    exports org.daniel.adventurelog.adventurelog;
-}
